2557d252d9a1ee1dc9a6984c6d680ee458dbe57e
171970160@qq.com <171970160@qq.com>
2017-09-23 16:23:45 +0800

    添加spring data jpa

---
+++ bp-utils/src/main/java/com/lhyzp/base/BaseService.java
@@ -0,0 +1,29 @@
+package com.lhyzp.base;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Map;
+
+/**
+ * 通用基础service
+ * Created by Administrator on 2017-9-22.
+ */
+public interface BaseService<T> {
+
+
+    /**
+     * jpa 分页查询
+     * @param condition
+     * @param pageable
+     * @return
+     */
+    Page<T> list(Map<String,Object> condition, Pageable pageable);
+
+    T save(T model);
+
+    void delete(Integer id);
+
+    T findById(Integer id);
+
+}
