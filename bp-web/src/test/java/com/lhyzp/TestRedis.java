8e6688739d6d0f9712c505eb78725d2dfb7067c9
171970160@qq.com <171970160@qq.com>
2017-09-26 18:10:40 +0800

    添加spring data jpa

--- bp-web/src/test/java/com/lhyzp/TestRedis.java
+++ bp-web/src/test/java/com/lhyzp/TestRedis.java
@@ -1,10 +1,17 @@
 package com.lhyzp;
 
 import com.google.common.collect.Maps;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Map;
 
@@ -38,5 +45,19 @@ public class TestRedis {
         if(BigDecimal.valueOf(500l).compareTo(BigDecimal.valueOf(100l))==1){
             System.out.println("大于成立");
         }
+    }
+
+    @Test
+    public void test3() throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault(); //创建httpclient
+        HttpGet httpGet = new HttpGet("http://www.lhyzp.com"); //创建get 请求
+        CloseableHttpResponse response = httpclient.execute(httpGet); //通过httpcleint 发送get 请求
+        if(response != null)
+        {
+            HttpEntity httpentity = response.getEntity();//获取响应
+            System.out.println(EntityUtils.toString(httpentity,"UTF-8")); //采用工具来将实体进行转换输出
+        }
+        response.close();
+        httpclient.close();
     }
 }
