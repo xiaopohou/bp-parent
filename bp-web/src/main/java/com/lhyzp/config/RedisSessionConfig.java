3619c4de4b8c33bf88915e230cc25fd36fd104fe
171970160@qq.com <171970160@qq.com>
2017-11-10 13:39:46 +0800

    添加spring data jpa

--- bp-web/src/main/java/com/lhyzp/config/RedisSessionConfig.java
+++ bp-web/src/main/java/com/lhyzp/config/RedisSessionConfig.java
@@ -1,15 +1,15 @@
 package com.lhyzp.config;
 
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
- * 分布式session   （必须要要有redis 不然启动报错）
+ * 启用redis缓存session （必须能连上redis 不然启动报错）
  * 做负载均衡时,session共享的解决方案
  * Created by Administrator on 2017-11-3.
  */
-//@Configuration
-//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60)//maxInactiveIntervalInSeconds: 设置Session失效时间，使用Redis Session之后，原Boot的server.session.timeout属性不再生效
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60)//maxInactiveIntervalInSeconds: 设置Session失效时间，使用Redis Session之后，原Boot的server.session.timeout属性不再生效
 public class RedisSessionConfig  {
 
 }
