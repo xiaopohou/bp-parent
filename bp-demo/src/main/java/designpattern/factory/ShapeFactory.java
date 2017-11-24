b4b6b2f4954e344920ecac063e8b5b261fff5f9e
171970160@qq.com <171970160@qq.com>
2017-11-24 18:00:19 +0800

    添加spring data jpa

--- bp-demo/src/main/java/designpattern/factory/ShapeFactory.java
+++ bp-demo/src/main/java/designpattern/factory/ShapeFactory.java
@@ -19,6 +19,7 @@ public class ShapeFactory {
     }
 
 
+    //使用反射机制可以解决每次增加一个产品时，都需要增加一个对象实现工厂的缺点
     public Object getShapeUp(Class<? extends Shape> clazz){
         Object obj=null;
         try {
