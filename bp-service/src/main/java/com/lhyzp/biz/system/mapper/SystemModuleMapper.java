6511b7385b156785e417d3e1f917515b9aeeff0e
171970160@qq.com <171970160@qq.com>
2017-12-22 16:28:15 +0800

    资源模块

---
+++ bp-service/src/main/java/com/lhyzp/biz/system/mapper/SystemModuleMapper.java
@@ -0,0 +1,10 @@
+package com.lhyzp.biz.system.mapper;
+
+import com.lhyzp.biz.system.model.SystemModule;
+import com.lhyzp.common.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SystemModuleMapper extends BaseMapper<SystemModule>{
+
+}
