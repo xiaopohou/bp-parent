e86eb9d1325ae40dc9fa10f81cbf6835be08a7c1
171970160@qq.com <171970160@qq.com>
2018-03-05 10:11:49 +0800

    add mybatis plugin

--- bp-common/src/main/java/com/lhyzp/common/exception/CustomException.java
+++ bp-common/src/main/java/com/lhyzp/common/exception/CustomException.java
@@ -4,7 +4,7 @@ package com.lhyzp.common.exception;
  * 自定义异常
  * Created by Administrator on 2017-12-22.
  */
-public class CustomException extends Exception{
+public class CustomException extends RuntimeException{
 
     public CustomException() {
     }
